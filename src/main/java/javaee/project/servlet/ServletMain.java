56527a2578ac64b029d67309a5f70210191b69b7
zhaoyuwy <zhaoyuwy@163.com>
2017-02-22 03:52:19 +0800

    gradle web 本地tomcat可运行

---
+++ src/main/java/javaee/project/servlet/ServletMain.java
@@ -0,0 +1,16 @@
+package javaee.project.servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2017/2/22.
+ */
+public class ServletMain extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
